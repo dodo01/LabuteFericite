df83fa70a4dce6d392eaa245f924127620b6e587
toader-adina <toader_elena_adina@yahoo.com>
2023-05-30 16:25:16 +0300

    Update Startup.cs

--- LabuteCalatoare.Business/Initialization/Startup.cs
+++ LabuteCalatoare.Business/Initialization/Startup.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using LabuteCalatoare.Business.Services.DatabaseServices;
+using LabuteCalatoare.Business.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace LabuteCalatoare.Business.Initialization
 {
     public class Startup
     {
-        private IServiceProvider _serviceProvider;
-        private readonly DataBase.Initialization.Startup dbStartup = new DataBase.Initialization.Startup();
- 
+
+        private static readonly DataBase.Initialization.Startup dbStartup = new DataBase.Initialization.Startup();
+
+        public void ConfigureServices(IServiceCollection services, string connectionString)
+        {
+            services.AddTransient<IHotelService, HotelService>();
+        }
     }
 }
